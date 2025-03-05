a565abd9ce7564cfddbb608c57018b84919f9447
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2025-03-05 17:42:20 +0700

    add draft contracts

---
+++ contracts/LpStrategy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "./interfaces/IStrategy.sol";
+
+contract LpStrategy is IStrategy {
+  address public router;
+  struct DepositParams {
+    uint256 tokenId;
+  }
+
+  function deposit(uint256 amount, bytes calldata data) external override returns (DepositDetails memory details) {
+  }
+
+  function getValueInPrinciple(DepositDetails memory details) external override returns (uint256) {
+    return details.amount;
+  }
+
+  function withdraw(uint256 amount) external returns (uint256) {
+  }
+
+  function compound(uint256 value) external returns (uint256) {
+  }
+}
---
+++ contracts/interfaces/IStrategy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+interface IStrategy {
+  struct DepositDetails {
+    address token;
+    uint256 tokenId;
+    uint256 amount;
+  }
+
+  function deposit(uint256 amount, bytes calldata data) external returns (DepositDetails memory);
+
+  function withdraw(uint256 shares) external returns (uint256 amount);
+
+  function getValueInPrinciple(DepositDetails calldata) external returns (uint256 amount);
+}
