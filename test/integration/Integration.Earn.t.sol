1963ecb6a65e374b22f0effc822c2f58907ea485
tungpun <htung.nht@gmail.com>
2025-04-01 10:12:26 +0700

    chore: updated Earn test

--- test/integration/Integration.Earn.t.sol
+++ test/integration/Integration.Earn.t.sol
@@ -21,7 +21,9 @@ import { Vault } from "../../contracts/core/Vault.sol";
 import { IVault } from "../../contracts/interfaces/core/IVault.sol";
 import { PoolOptimalSwapper } from "../../contracts/core/PoolOptimalSwapper.sol";
 import { LpStrategy } from "../../contracts/strategies/lpUniV3/LpStrategy.sol";
+import { LpValidator } from "../../contracts/strategies/lpUniV3/LpValidator.sol";
 import { ILpStrategy } from "../../contracts/interfaces/strategies/ILpStrategy.sol";
+import { ILpValidator } from "../../contracts/interfaces/strategies/ILpValidator.sol";
 
 contract IntegrationTest is TestCommon {
   ConfigManager public configManager;
@@ -35,15 +37,14 @@ contract IntegrationTest is TestCommon {
 
   function setUp() public {
     console.log("Setting up the vault...");
-    
-    uint256 fork = vm.createFork(vm.envString("RPC_URL"), 28_315_545);
-    vm.selectFork(fork);    
+
+    uint256 fork = vm.createFork(vm.envString("RPC_URL"), 27_448_360);
+    vm.selectFork(fork);
 
     setErc20Balance(WETH, USER, 1 ether);
     setErc20Balance(WETH, PLAYER_1, 1 ether);
     setErc20Balance(WETH, PLAYER_2, 1 ether);
     setErc20Balance(WETH, BIGHAND_PLAYER, 0 ether);
-
     swap_amount = 4_350_000_000_000;
     setErc20Balance(USDC, BIGHAND_PLAYER, swap_amount);
 
@@ -55,38 +56,39 @@ contract IntegrationTest is TestCommon {
     typedTokens[1] = USDC;
 
     uint256[] memory typedTokenTypes = new uint256[](2);
-    typedTokenTypes[0] = uint256(ILpStrategy.TokenType.Stable);
-    typedTokenTypes[1] = uint256(ILpStrategy.TokenType.Stable);
+    typedTokenTypes[0] = uint256(ILpValidator.TokenType.Stable);
+    typedTokenTypes[1] = uint256(ILpValidator.TokenType.Stable);
 
     address[] memory whitelistAutomator = new address[](1);
     whitelistAutomator[0] = USER;
 
-    configManager = new ConfigManager(USER, whitelistAutomator, typedTokens, typedTokenTypes);    
+    configManager = new ConfigManager(USER, whitelistAutomator, typedTokens, typedTokenTypes);
 
     PoolOptimalSwapper swapper = new PoolOptimalSwapper();
-    lpStrategy = new LpStrategy(address(swapper), address(configManager));    
+    LpValidator validator = new LpValidator(address(configManager));
+    lpStrategy = new LpStrategy(address(swapper), address(validator));
 
     address[] memory strategies = new address[](1);
     strategies[0] = address(lpStrategy);
     console.log("strategies: ", address(strategies[0]));
-    
+
     vm.startPrank(USER);
     configManager.whitelistStrategy(strategies, true);
     console.log("configManager.whitelistStrategy(strategies, true)");
 
-    ILpStrategy.LpStrategyConfig memory initialConfig = ILpStrategy.LpStrategyConfig({
-      rangeConfigs: new ILpStrategy.LpStrategyRangeConfig[](1),
-      tvlConfigs: new ILpStrategy.LpStrategyTvlConfig[](1)
-    });    
+    ILpValidator.LpStrategyConfig memory initialConfig = ILpValidator.LpStrategyConfig({
+      rangeConfigs: new ILpValidator.LpStrategyRangeConfig[](1),
+      tvlConfigs: new ILpValidator.LpStrategyTvlConfig[](1)
+    });
 
     initialConfig.rangeConfigs[0] =
-      ILpStrategy.LpStrategyRangeConfig({ tickWidthMultiplierMin: 3, tickWidthStableMultiplierMin: 3 });
+      ILpValidator.LpStrategyRangeConfig({ tickWidthMultiplierMin: 3, tickWidthStableMultiplierMin: 3 });
 
-    initialConfig.tvlConfigs[0] = ILpStrategy.LpStrategyTvlConfig({ principalTokenAmountMin: 0.1 ether });
+    initialConfig.tvlConfigs[0] = ILpValidator.LpStrategyTvlConfig({ principalTokenAmountMin: 0.1 ether });
 
     vm.startPrank(USER);
-    configManager.setStrategyConfig(address(lpStrategy), WETH, abi.encode(initialConfig));
-    console.log("configManager.setStrategyConfig(address(lpStrategy), WETH, abi.encode(initialConfig))");
+    configManager.setStrategyConfig(address(validator), WETH, abi.encode(initialConfig));
+    console.log("configManager.setStrategyConfig(address(validator), WETH, abi.encode(initialConfig))");
 
     // Set up VaultFactory
     vaultImplementation = new Vault();
@@ -115,7 +117,7 @@ contract IntegrationTest is TestCommon {
 
     vaultInstance = Vault(payable(vaultAddress));
     console.log("vaultInstance: ", address(vaultInstance));
-  }  
+  }
 
   function test_earning() public {    
     
@@ -130,7 +132,7 @@ contract IntegrationTest is TestCommon {
     console.log("usdc balance of the owner (before depositing): ", IERC20(USDC).balanceOf(USER));
     vaultInstance.deposit(1 ether, 0);
     vaultAssets = vaultInstance.getInventory();
-    
+
     console.log("==== Owner is allocating 1 ether to a new LP position ====");
     AssetLib.Asset[] memory assets = new AssetLib.Asset[](1);
     assets[0] = AssetLib.Asset(AssetLib.AssetType.ERC20, address(0), WETH, 0, 0.95 ether);
@@ -153,6 +155,8 @@ contract IntegrationTest is TestCommon {
     vaultInstance.allocate(assets, lpStrategy, 0, abi.encode(instruction));
     assertEq(vaultInstance.balanceOf(USER), 10000000000000000000000);
 
+    console.log("total value of the vault (after allocation): ", vaultInstance.getTotalValue());
+
     console.log("==== Player 1 is depositing 1 ether to the vault ====");
     vm.startPrank(PLAYER_1);
     IERC20(WETH).approve(address(vaultInstance), 1 ether);
@@ -161,7 +165,7 @@ contract IntegrationTest is TestCommon {
 
     console.log("++ share balance of the player: ", vaultInstance.balanceOf(PLAYER_1));
     console.log("++ share balance of the owner: ", vaultInstance.balanceOf(USER));
-    console.log("++ total value of the vault: ", vaultInstance.getTotalValue());
+    console.log("++ total value of the vault (before the swap): ", vaultInstance.getTotalValue());
 
     (,, address token0, address token1, uint24 fee,,,,,,,) = INFPM(NFPM).positions(vaultInstance.getInventory()[2].tokenId);    
     address pool = IUniswapV3Factory(INFPM(NFPM).factory()).getPool(token0, token1, fee);    
@@ -193,18 +197,16 @@ contract IntegrationTest is TestCommon {
 
     console.log("----------------------------- Ended a round of swap");
     
-
-    console.log("++ total value of the vault: ", vaultInstance.getTotalValue());
+    console.log("++ total value of the vault (after the swap): ", vaultInstance.getTotalValue());
   
     console.log("==== Player 1 is withdrawing all from the vault ====");
     vm.startPrank(PLAYER_1);
     vaultInstance.withdraw(vaultInstance.balanceOf(PLAYER_1), false, 0);
 
-    console.log("p1 weth balance: ", IERC20(WETH).balanceOf(PLAYER_1));
-
     uint256 dollar_lost_player_1 = (p1_old_weth_balance - IERC20(WETH).balanceOf(PLAYER_1)) * 2000 / 10 ** 12;  // given the ETH price is 2000    
     uint256 dollar_lost_bighand_player = swap_amount - IERC20(USDC).balanceOf(BIGHAND_PLAYER);
     
+    console.log("++ total value of the vault (after player 1 withdrawing): ", vaultInstance.getTotalValue());
 
     // assert(IERC20(WETH).balanceOf(PLAYER_1) > p1_old_weth_balance);
     // assert(dollar_lost_player_1 < dollar_lost_bighand_player);
@@ -218,9 +220,6 @@ contract IntegrationTest is TestCommon {
     // assertEq(vaultInstance.balanceOf(USER), 0);
     // assertEq(vaultInstance.balanceOf(PLAYER_1), 0);    
     
-    console.log("weth balance of the owner: ", IERC20(WETH).balanceOf(USER));
-    console.log("usdc balance of the owner: ", IERC20(USDC).balanceOf(USER));
-
     console.log(">>> Summary of case: swapping %d USDC -> wETH <<<", swap_amount);
 
     console.log(">>> weth loss of the player 1: ", p1_old_weth_balance - IERC20(WETH).balanceOf(PLAYER_1));    
