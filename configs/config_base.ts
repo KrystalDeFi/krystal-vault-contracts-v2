886f3136ee23c02ed3e11c60b8543a551ba07a74
alonecandies <dragonfrezze@gmail.com>
2025-04-14 17:36:00 +0700

    chore: update contracts

--- configs/config_base.ts
+++ configs/config_base.ts
@@ -37,8 +37,12 @@ export const BaseConfig: Record<string, IConfig> = {
       "0xfde4c96c8593536e31f229ea8f37b2ada2699bb2", // USDT
       "0x50c5725949a6f0c72e6c4a641f24049a917db0cb", // DAI
       "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca", // USDBC
+      "0x820c137fa70c8691f0e44dc420a5e53c168921dc", // USDS
       "0x4200000000000000000000000000000000000006", // WETH
       "0xc1cba3fcea344f92d9239c08c0568f6f2f0ee452", // wstETH
+      "0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22", // CBETH
+      "0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c", // RETH
+      "0xb29749498954a3a821ec37bde86e386df3ce30b6", // LSETH
     ],
     // 1 for stable, 2 for ETH,...
     typedTokensTypes: [
