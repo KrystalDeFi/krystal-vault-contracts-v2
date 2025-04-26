8de7e2702f671d289d247afb3e9ce1825b9d617a
tungpun <htung.nht@gmail.com>
2025-04-26 10:04:40 +0700

    chore: updated running script

--- run-echidna-test.sh
+++ run-echidna-test.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-echo "[+] Clean the echidna-fuzzer contract in the contracts directory"
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+clear='\033[0m'
+
+function log_info() {
+    echo -e "${magenta}$1${clear}"
+}
+
+function log_error() {
+    echo -e "${red}$1${clear}"
+}
+
+log_info "[+] Clean the echidna-fuzzer contract in the contracts directory"
 rm -rf contracts/echidna-fuzzer
 
-echo "[+] Move the foundry.toml file to foundry.toml.disabled to use hardhat as the compiler only"
+log_info "[+] Move the foundry.toml file to foundry.toml.disabled to use hardhat as the compiler only"
 mv foundry.toml foundry.toml.disabled
 
-echo "[+] Copy the echidna-fuzzer contract to the contracts directory"
+log_info "[+] Copy the echidna-fuzzer contract to the contracts directory"
 mkdir contracts/echidna-fuzzer/
-find test/echidna-fuzzer -type f -not -name "*.forge.sol" -exec cp -v {} contracts/echidna-fuzzer/ \;
+find test/echidna-fuzzer -type f -not -name "*.t.sol" -exec cp -v {} contracts/echidna-fuzzer/ \;
 
-echo "[+] Comment out all console.log lines in the contracts"
-find contracts/echidna-fuzzer -type f -name "*.sol" -exec sed -i '' 's/^[[:space:]]*console\.log/\/\/ console\.log/g' {} \;
+log_info "[+] Comment out all console.log lines in the contracts"
+find contracts/ -type f -name "*.sol" -exec sed -i '' 's/^[[:space:]]*console\.log/\/\/ console\.log/g' {} \;
 
-echo "[+] Comment out all forge-test-only lines in the contracts"
-find contracts/echidna-fuzzer -type f -name "*.sol" -exec sed -i '' 's/^[[:space:]]*.*\/\/forge-test-only.*$/\/\/ &/g' {} \;
+log_info "[+] Comment out all forge-test-only lines in the contracts"
+find contracts/ -type f -name "*.sol" -exec sed -i '' 's/^[[:space:]]*.*\/\/forge-test-only.*$/\/\/ &/g' {} \;
 
-echo "[+] Run the echidna test: echidna ./ --config test/echidna-fuzzer/config.yaml --contract VaultFuzzer"
+log_info "[+] Comment out all forge-std lines in the contracts"
+find contracts/ -type f -name "*.sol" -exec sed -i '' 's/^[[:space:]]*.*forge-std\/.*$/\/\/ &/g' {} \;
+
+log_info "[+] Run the echidna test: echidna ./ --config test/echidna-fuzzer/config.yaml --contract VaultFuzzer"
 echidna ./ --config test/echidna-fuzzer/config.yaml --contract VaultFuzzer
 
-echo "[+] Restore the foundry.toml file"
+log_info "[+] Restore the foundry.toml file"
 mv foundry.toml.disabled foundry.toml
 
-echo "[+] Remove the echidna-fuzzer contract in the contracts directory"
+log_info "[+] Remove the echidna-fuzzer contract in the contracts directory"
 rm -rf contracts/echidna-fuzzer
