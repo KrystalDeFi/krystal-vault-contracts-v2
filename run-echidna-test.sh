95d92312c65c48d14cfa90f16e4878171058f860
tungpun <htung.nht@gmail.com>
2025-05-06 17:56:19 +0700

    chore: updated script

--- run-echidna-test.sh
+++ run-echidna-test.sh
@@ -17,11 +17,18 @@ function log_error() {
     echo -e "${red}$1${clear}"
 }
 
+log_info "[+] Clean the out directory"
+rm -rf out/
+
 log_info "[+] Clean the echidna-fuzzer contract in the contracts directory"
 rm -rf contracts/echidna-fuzzer
 
-log_info "[+] Move the foundry.toml file to foundry.toml.disabled to use hardhat as the compiler only"
-mv foundry.toml foundry.toml.disabled
+log_info "[+] Move the hardhat.config.ts file to hardhat.config.ts.disabled to use foundry as the compiler only"
+mv hardhat.config.ts hardhat.config.ts.disabled
+
+log_info "[+] Replace the out directory in foundry.toml"
+cp foundry.toml foundry.toml.tmp
+sed -i '' 's/out = '\''artifacts'\''/out = '\''out\/'\''/g' foundry.toml
 
 log_info "[+] Copy the echidna-fuzzer contract to the contracts directory"
 mkdir contracts/echidna-fuzzer/
@@ -37,13 +44,16 @@ log_info "[+] Comment out all forge-std lines in the contracts"
 find contracts/ -type f -name "*.sol" -exec sed -i '' 's/^import[[:space:]]\"forge-std\/.*/\/\/ &/g' {} \;
 
 export ECHIDNA_RPC_URL=https://rpc.ankr.com/eth/431b8fcced2be35b5b757fc266beb9f70373e23bc8bd715c31759b1fdf50ad8a
-export ECHIDNA_RPC_BLOCK=22365182 
 
 log_info "[+] Run the echidna test: echidna ./ --config test/echidna-fuzzer/config.yaml --contract VaultFuzzer"
 echidna ./ --config test/echidna-fuzzer/config.yaml --contract VaultFuzzer
 
+log_info "[+] Restore the hardhat.config.ts file"
+mv hardhat.config.ts.disabled hardhat.config.ts
+
 log_info "[+] Restore the foundry.toml file"
-mv foundry.toml.disabled foundry.toml
+mv foundry.toml.tmp foundry.toml
 
 log_info "[+] Remove the echidna-fuzzer contract in the contracts directory"
 rm -rf contracts/echidna-fuzzer
+
