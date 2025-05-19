475f362f4c6a56208d1dd988a1554ab6798fa47f
Long Hoang <dragonfrezze@gmail.com>
2025-05-19 21:35:27 +0700

    Update Integration.Katana.t.sol

--- test/integration/Integration.Katana.t.sol
+++ test/integration/Integration.Katana.t.sol
@@ -41,7 +41,7 @@ contract IntegrationTest is TestCommon {
   Vault public vaultInstance;
 
   function setUp() public {
-    uint256 fork = vm.createFork("https://ronin.drpc.org", 45_264_950);
+    uint256 fork = vm.createFork("https://ronin.lgns.net/rpc", 45_264_950);
     vm.selectFork(fork);
 
     vm.startBroadcast(USER);
