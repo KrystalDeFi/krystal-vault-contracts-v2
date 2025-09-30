22dbb68d67b08e9e15b29b71409badd86923bc0a
Nguyen Khoi Nguyen <nguyennk92@gmail.com>
2025-09-30 14:22:35 +0700

    add aerodrome farming

---
+++ contracts/private-vault/strategies/farm/AerodromeFarmingStrategy.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+import { ICLGauge } from "../../interfaces/strategies/aerodrome/ICLGauge.sol";
+import { IERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract AerodromeFarmingStrategy {
+  function deposit(uint256 tokenId, address clGauge) external {
+    address nfpm = ICLGauge(clGauge).nft();
+    if (tokenId == 0) {
+      // deposit the last created token
+      tokenId = IERC721Enumerable(nfpm).totalSupply() - 1;
+    }
+    IERC721(nfpm).approve(clGauge, tokenId);
+    ICLGauge(clGauge).deposit(tokenId);
+  }
+
+  function withdraw(uint256 tokenId, address clGauge) external {
+    ICLGauge(clGauge).withdraw(tokenId);
+  }
+
+  function harvest(address clGauge, uint256 tokenId) external {
+    ICLGauge(clGauge).getReward(tokenId);
+  }
+
+  function harvestAll(address clGauge) external {
+    ICLGauge(clGauge).getReward(address(this));
+  }
+}
